784f7e3ad851cbbd2718733c431c30203007a530
kkiriyama <nobuhiro.kurata@coinotaku.com>
2019-03-24 05:10:41 +0900

    initial commit for api

---
+++ api/routes/timeline.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const admin = require('firebase-admin')
+const cors = require('cors')
+
+var serviceAccount = require("../.firestoreconfig/tsuratsura-0000-firebase-adminsdk-8wioi-c0cb2e6460.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+var db = admin.firestore()
+
+router.get('/', cors(), function(req, res, next) {
+    (async () => {
+        const data = await db.collection('posts').get()
+        let postData = []
+        data._docs().forEach(doc => postData.push(doc._fieldsProto))
+        res.send({
+                data: postData
+        })
+    })().catch(next)
+});
+
+module.exports = router;
