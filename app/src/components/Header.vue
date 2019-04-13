3c51ddd94dcd9158222597d30190614e6d1c2f25
kkiriyama <kurata-nobuhiro901@g.ecc.u-tokyo.ac.jp>
2019-04-13 13:07:40 +0900

    fix page width

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar toggleable="md" type="dark" variant="info">
             <b-navbar-brand><router-link class="header-link" to="/"> TsuraTsura ~つらいひとのSNS~ </router-link></b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
